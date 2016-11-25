c59529f8a6e7af111ca59e834bd5baf69b38d924
Ivan Doležal <ivan.dolezal@gmail.com>
2016-11-25 16:09:52 +0100

    From file to file

--- src/app.js
+++ src/app.js
@@ -1,39 +1,59 @@
 import rq from 'request-promise-native';
+import readline from 'readline';
+import fs from 'fs';
+
 
 require('babel-polyfill');
 
 const geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
 const apiKey = process.argv[2]; // Google maps API key as script argument
 
-const addresses = ['Jungmannova 1405, Hradec Králové', 'Náměstí 28. Října, Hradec Králové', ''];
+const inFile = process.argv[3] || './inFile.csv';
+const outFile = process.argv[4] || './outFile.csv';
+
+const separator = ';';
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream(inFile)
+});
+
+const lineWriter = fs.createWriteStream(outFile);
 
 async function geocodeAddress(address) {
     const url = geocodeUrl + encodeURIComponent(address) + '&key=' + apiKey;
     try {
-        let res = await rq.get({url: url});
+        let res = await rq.get({ url: url });
         let data = JSON.parse(res);
-        if(data.status === 'OK') {
+        if (data.status === 'OK') {
             return data.results[0].geometry.location;
+        } else {
+            return Promise.reject(new Error(data.status));
         }
     } catch (err) {
-        new Promise.reject(err);
+        return Promise.reject(err);
     }
 }
 
-async function geocode(addresses) {
-    for (let address of addresses) {
+async function geocode() {
+    const regex = /\(.*\)/g; //
+    lineReader.on('line', async(line) => {
+        let agent = line.split(separator);
+        let address = agent[5] + ' ' + agent[6] + ', ' + agent[7];
+        address = address.replace(regex, ''); // Remove () place specification from address
+        let id = agent[18];
         try {
-            console.log(`Checking  "${address}"...`);
             let location = await geocodeAddress(address);
-            console.log(`Address  "${address}" checked`);
-            console.log(`LAT: ${location.lat}`);
-            console.log(`LNG: ${location.lng}`);
+            lineWriter.write([id, location.lng, location.lat].join(separator) + '\n');
         } catch (err) {
-            console.error(`Error obtaining location for "${address}"`);
+            console.error(`Error obtaining location for ${id}. Address - ${address}`);
             console.error(err);
         }
-
-    }
+    });
+}
+async function main() {
+    console.log(`Geocoding data from ${inFile}`);
+    await geocode();
 }
 
-geocode(addresses);
+main(lineReader, lineWriter);
